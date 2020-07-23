bcb85a0a2625332d80b66c885ae0fc1c26a4a750
Mateusz Górski <mateuszg122@gmail.com>
2020-07-23 17:25:06 +0200

    random cycle not finished

--- src/experiments_helpers.py
+++ src/experiments_helpers.py
@@ -9,8 +9,10 @@ import numpy as np
 from time import time
 
 
-def random_cycle_paths_out(max_cycle_len, max_path_len, max_vertices, min_cycle_len=1):
-    '''Funkcja losująca grafy nieskierowane o dokładnie jedym cyklu, z którego wychodzą rozłączne ścieżki.
+def random_cycle_paths_out(max_cycle_len, max_path_len, max_vertices, paths_type, min_cycle_len=1):
+    '''Funkcja losująca grafy nieskierowane o dokładnie jedym cyklu, z którego wychodzą ścieżki dwóch typów.
+    Pierwszym z nich jest typ "zewnętrzny", czyli ścieżki, które zaczynają się w jednym z wierzchołków cyklu i których
+    pozostałe wierzchołki są z cyklem rozłączne. Drugi typ jest "wewnętrzny" TODO dokończyć opis
 
     :param min_cycle_len: Int
         Minimalna dozwolona długość cyklu.
@@ -19,17 +21,19 @@ def random_cycle_paths_out(max_cycle_len, max_path_len, max_vertices, min_cycle_
         wierzchołek, a jeżeli będzie równa `2`, to wygenerowana zostanie krawędź.
     :param max_path_len: Int
         Największa dozwolona długość ścieżki wychodzącej z cyklu.
+    :param p: Float
+        Określa, z jakim prawdopodobieństwem do grafu zostanie dodana kolejna ścieżka wychodząca.
     :param max_vertices: Int
         Największa dozwolona liczba krawędzi.
     :return: tuple
-        Para skłądająca się z opisanego grafu skierowanego, oraz długości jego najdłuższej ścieżki.
+        Para składająca się z opisanego grafu skierowanego, oraz długości jego najdłuższej ścieżki.
     '''
     if min_cycle_len < 1:
         raise ValueError("Minimalna długość cyklu nie może być mniejsza od 1.")
     if min_cycle_len > max_cycle_len:
         raise ValueError("Minimalna długość cyklu musi być mniejsza lub równa maksymalnej.")
     G = Graph()
-    cycle_len = np.random.randint(1, max_cycle_len) # [3, max_path_len + 2)
+    cycle_len = np.random.randint(min_cycle_len, max_cycle_len + 1)
     if cycle_len == 1:
         G.add_vertex(0)
     elif cycle_len == 2:
@@ -51,6 +55,47 @@ def random_cycle_paths_out(max_cycle_len, max_path_len, max_vertices, min_cycle_
             n += path_length
             if got_max_vertices:
                 break
+    return G
+
+
+def random_cycle_paths_inside(max_cycle_len, max_path_len, max_vertices, min_cycle_len=1):
+
+
+
+def random_multiple_cycles_connected(n_cycles, max_vertices, max_cycle_len, max_path_len, min_cycle_len=1):
+    '''Generuje `n_cycles` rozłącznch cykli według procedury opisanej w `random_cycle_paths_out`. Następnie łączy
+    te cykle poprzez scalanie kolejnych losowo wybranych wierzchołków, dopóki graf nie będzie spójny.
+
+    :param n_cycles: Int
+        Liczba wygenerowanych rozłącznych cykli.
+    :param max_vertices: Int
+        Maksymalna dozwolona liczba wierzchołków w zwracanym grafie.
+    :param max_cycle_len: Int
+        Maksymalna dozwolona długość cyklu spośród tych generowanych na początku. Ostateczny graf może posiadać dłuższe
+        cykle!
+    :param max_path_len: Int
+        Maksymalna dozwolona długość ścieżki wychodzącej z pojedynczego cyklu. Określa jedynie długości ścieżek w
+        cyklach generowanych na samym początku, a nie w ostatecznym grafie.
+    :param min_cycle_len:
+        Minimalna długość generowanych cykli.
+    :return:
+        Spójny graf nieskierowany
+    '''
+    G = Graph()
+    for i in range(n_cycles):
+        H = random_cycle_paths_out(max_cycle_len=max_cycle_len,
+                                   min_cycle_len=min_cycle_len,
+                                   max_path_len=int(max_path_len/2),
+                                   max_vertices=int(max_vertices/n_cycles))
+        G = G.disjoint_union(H)
+    G.relabel()
+    connected_components = G.connected_components()
+    while len(connected_components) > 1:
+        i, j = np.random.choice(range(len(connected_components)), size=2, replace=False)
+        v, = np.random.choice(connected_components[i], size=1)
+        u, = np.random.choice(connected_components[j], size=1)
+        G.merge_vertices([u, v])
+        connected_components = G.connected_components()
     return G
 
 
